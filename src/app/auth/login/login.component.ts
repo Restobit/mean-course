2e39310a8e8c98838a6daa5f274fa19333c650af
Fekete Otto <fekete.otto@jacsomedia.hu>
2021-10-14 16:32:56 +0200

    auth

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "../auth.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+
+
+export class LoginComponent {
+  isLoading = false;
+
+  constructor(public authService: AuthService) {}
+
+  onLogin(loginForm: NgForm){
+   if (loginForm.invalid){
+     return;
+   }
+
+   this.authService.login(loginForm.value.email, loginForm.value.password);
+
+  }
+}
